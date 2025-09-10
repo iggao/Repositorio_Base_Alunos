91a5d7a0f0f98fabf6a22f9acba17699e6da3901
I Can't Breathe <paomacinha321@gmail.com>
2025-09-10 16:39:58 -0300

    Loopings

---
+++ Modulo 1/Loopings/1.1.tabuada.py
@@ -0,0 +1,7 @@
+numero = int(input('digite um numero de 1 a 10: '))
+
+#sintaxe do for "for i in range(inicio, fim):"
+#comando a ser executado
+
+for i in range(1, 11):
+    print(f"{i} x {numero} = {i * numero}")
---
+++ Modulo 1/Loopings/1.2.tabuada.py
@@ -0,0 +1,6 @@
+numero = int(input('digite um numero de 1 a 10: '))
+i = 1
+
+while i <= 10:
+    print(f"{i} x {numero} = {i * numero}")
+    i += 1
